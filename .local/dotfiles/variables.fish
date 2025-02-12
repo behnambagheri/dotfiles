e284aea39c9130b771af9a285acbc0f2136ef2b1
Behnam Bagheri <bagheri@linuxmail.org>
2025-02-12 12:36:24 +0330

    Commit files:

--- .local/dotfiles/variables.fish
+++ .local/dotfiles/variables.fish
@@ -1,4 +1,4 @@
 # Exclude SSH sessions from notifications
-set -U __done_exclude '^git (?!push|pull|fetch)'  '^ssh' '^vim' '^sudo -Es'
+set -U __done_exclude '^git (?!push|pull|fetch)'  '^ssh' '^vim' '^sudo -Es' '^sudo su -'
 set -U __done_min_cmd_duration 5000  # default: 5000 ms
 set -U __done_allow_nongraphical 1
