5605e0caa0711ab2577308019151134efab49cd9
Behnam Bagheri <bagheri@linuxmail.org>
2025-02-16 02:20:01 +0330

    Commit files: ../../../../../../../.config/fish/functions/config-update.fish

--- .config/fish/functions/config-update.fish
+++ .config/fish/functions/config-update.fish
@@ -1,3 +1,30 @@
 function config-update
-config pull && bash ~/.local/dotfiles/config_installer.sh 
+    set use_proxy false
+    set use_public_proxy false
+
+    # Parse arguments
+    for arg in $argv
+        if test "$arg" = "--with-proxy"
+            set use_proxy true
+        else if test "$arg" = "--public-proxy"
+            set use_public_proxy true
+        end
+    end
+
+    # Run the config pull command
+    config pull
+
+    # Execute the main script
+    bash ~/.local/dotfiles/config_installer.sh
+
+    # Apply proxy settings if needed
+    if test "$use_proxy" = true
+        echo "Applying proxy settings..."
+        # Add proxy setup commands here
+    end
+
+    if test "$use_public_proxy" = true
+        echo "Applying public proxy settings..."
+        # Add public proxy setup commands here
+    end
 end
