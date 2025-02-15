553c8a301ef248961247e0b01cf8043edc62b2d2
Behnam Bagheri <bagheri@linuxmail.org>
2025-02-16 02:01:45 +0330

    Commit files: .config/fish/functions/ipinfo.fish

--- .config/fish/functions/ipinfo.fish
+++ .config/fish/functions/ipinfo.fish
@@ -1,3 +1,8 @@
 function ipinfo
-curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$argv" | jq .
+if string match -q -r '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' -- $argv[1]
+    curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$argv[1]" | jq .
+else
+    set ip (dig +short $argv[1] | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -1)
+    curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$ip" | jq .
+end
 end
