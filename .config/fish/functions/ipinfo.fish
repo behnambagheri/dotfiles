e5311dd977dd5c0439bface3bff1e184c0238ff1
Behnam Bagheri <bagheri@linuxmail.org>
2025-02-16 02:08:26 +0330

    Commit files: ../../../../../../../.config/fish/functions/ipinfo.fish

--- .config/fish/functions/ipinfo.fish
+++ .config/fish/functions/ipinfo.fish
@@ -1,8 +1,8 @@
 function ipinfo
 if string match -q -r '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' -- $argv[1]
-    curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$argv[1]" | jq .
+    curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$argv[1]" | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude)' 
 else
     set ip (dig +short $argv[1] | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -1)
-    curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$ip" | jq .
+    curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$ip" | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude)' 
 end
 end
