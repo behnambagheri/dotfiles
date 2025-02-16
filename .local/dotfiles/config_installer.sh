31238549e829945280b84e42e03b471924595b9a
Behnam Bagheri <bagheri@linuxmail.org>
2025-02-16 19:36:45 +0330

    Commit files: ../../../../.local/dotfiles/config_installer.sh

--- .local/dotfiles/config_installer.sh
+++ .local/dotfiles/config_installer.sh
@@ -385,7 +385,6 @@ fi
 # Activate the virtual environment
 echo "Activating virtual environment..."
 source "$VENV_PATH/bin/activate"
-sudo chown -R $(id -u):$(id -g) "$HOME/.local/share/nvim"
 
 # Install the Neovim Python package
 echo "Installing Neovim Python package..."
@@ -405,17 +404,16 @@ echo 'export PATH="$HOME/.npm-global/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
 echo "Installing Vim-Plug for Neovim..."
-sudo chown -R $(id -u):$(id -g) "$HOME/.local/share/nvim"
 
 # Install Vim-Plug if not already installed
 echo "Installing Vim-Plug..."
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo chown -R $(id -u):$(id -g) "$HOME/.local/share/nvim"
 
 echo "Installing Vim plugins..."
+sudo chown -R $(id -u):$(id -g) "$HOME/.local/state/nvim/"
+
 nvim --headless +PlugInstall +qall
-sudo chown -R $(id -u):$(id -g) "$HOME/.local/share/nvim"
 
 # Ensure Neovim and coc.nvim are installed before running CocInstall
 echo "Installing coc.nvim extensions..."
