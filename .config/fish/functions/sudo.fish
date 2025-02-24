ee07f69a3f929ecd521f959b304aa8cd5d38a185
Behnam Bagheri <bagheri@linuxmail.org>
2025-02-24 18:26:22 +0330

    Commit files: ../../../../.config/fish/functions/sudo.fish ../../../../.local/dotfiles/variables.fish

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,3 +1,7 @@
 function sudo
-    command sudo -E $argv
+    if test "$argv[1]" = "vim"
+        command sudo -E nvim $argv[2..-1]  # Run nvim instead of vim while preserving the environment
+    else
+        command sudo -E $argv  # Run all other sudo commands normally while preserving the environment
+    end
 end
--- .local/dotfiles/variables.fish
+++ .local/dotfiles/variables.fish
@@ -1,5 +1,5 @@
 # Exclude SSH sessions from notifications
-set -U __done_exclude '^git (?!push|pull|fetch)'  '^ssh' '^vim'   '^sudo -Es' '^sudo su -' '^sudo vim' '^sudo crontab' '^crontab' '^watch' '^tail' '^sudo tail' '^kubectl exec' '^docker logs' '^docker compose logs' '^sudo docker logs' '^sudo docker compose logs' '^nvim' '^kubectl logs -f' '^kubectl get pods -o wide --watch' '^sudo -s$' '^htop$' '^glances$' '^bash$'
+set -U __done_exclude '^git (?!push|pull|fetch)'  '^ssh' '^vim'   '^sudo -Es' '^sudo su -' '^sudo vim' '^sudo crontab' '^crontab' '^watch' '^tail' '^sudo tail' '^kubectl exec' '^docker logs' '^docker compose logs' '^sudo docker logs' '^sudo docker compose logs' '^nvim' '^kubectl logs -f' '^kubectl get pods -o wide --watch' '^sudo -s$' '^htop$' '^glances$' '^bash$' '^sudo nvim'
 # set -U __done_exclude '^(git (?!push|pull|fetch)|ssh|vim|nvim|sudo -Es|sudo su -|sudo (vim|crontab)|crontab|watch|tail|sudo tail|kubectl exec|docker (logs|compose logs)|sudo docker (logs|compose logs)|less|more|cat|journalctl|dmesg|htop|top|iotop|bmon|glances|man|ncdu|tmux|screen|nvim)$'
 set -U __done_min_cmd_duration 29000  # default: 5000 ms
 set -U __done_allow_nongraphical 1
