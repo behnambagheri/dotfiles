d5a3eaf6501936d9e1b703b98193c3111aef0388
Behnam Bagheri <bagheri@linuxmail.org>
2025-03-04 16:57:51 +0330

    Commit files: .config/fish/functions/config-update.fish .config/fish/functions/ipinfo.fish

--- .config/fish/functions/config-update.fish
+++ .config/fish/functions/config-update.fish
@@ -1,3 +1,3 @@
 function config-update
-config pull && bash ~/.local/dotfiles/config_installer.sh $argv 
+    config pull && bash ~/.local/dotfiles/config_installer.sh && fisher remove meaningful-ooo/sponge gazorby/fish-abbreviation-tips 2> /dev/null
 end
--- .config/fish/functions/ipinfo.fish
+++ .config/fish/functions/ipinfo.fish
@@ -1,8 +1,28 @@
 function ipinfo
-if string match -q -r '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' -- $argv[1]
-    curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$argv[1]" | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude)' 
-else
-    set ip (dig +short $argv[1] | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -1)
-    curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$ip" | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude)' 
-end
+    if test (count $argv) -eq 0
+        # Fetch data from both APIs
+        set data1 (curl -s -H 'Accept: application/json' "ip.bea.sh")
+        set data2 (curl -s -H 'Accept: application/json' "ident.me/json")
+        
+        # Extract IP addresses
+        set ip1 (echo $data1 | jq -r '.ip')
+        set ip2 (echo $data2 | jq -r '.ip')
+        
+        # Compare IPs and format JSON output
+        if test "$ip1" = "$ip2"
+            echo '{
+  "ip.bea.sh":' (echo $data1 | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude, .time_zone, .asn)') '
+}' | jq .
+        else
+            echo '{
+  "ip.bea.sh":' (echo $data1 | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude, .time_zone, .asn)') ',
+  "ident.me":' (echo $data2 | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude, .tz, .weather, .postal, .asn, .continent)') '
+}' | jq .
+        end
+    else if string match -q -r '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' -- $argv[1]
+        curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$argv[1]" | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude, .time_zone, .asn)' | jq .
+    else
+        set ip (dig +short $argv[1] | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | head -1)
+        curl -s -H 'Accept: application/json' "ip.bea.sh/?ip=$ip" | jq 'del(.user_agent, .ip_decimal, .country_eu, .region_code, .latitude, .longitude, .time_zone, .asn)' | jq .
+    end
 end
