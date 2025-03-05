c476f3927c00adf43bccdc3c894cc91fd965f0a9
Behnam Bagheri <bagheri@linuxmail.org>
2025-03-05 13:56:37 +0330

    Commit files: .config/fish/functions/config-update.fish

--- .config/fish/functions/config-update.fish
+++ .config/fish/functions/config-update.fish
@@ -1,5 +1,5 @@
 function config-update
     config pull
     bash ~/.local/dotfiles/config_installer.sh $argv
-    fisher remove meaningful-ooo/sponge gazorby/fish-abbreviation-tips 2> /dev/null; or true
+    fisher remove meaningful-ooo/sponge gazorby/fish-abbreviation-tips 2> /dev/null || true
 end
