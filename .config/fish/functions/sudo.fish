0cbd2318eb20052905f9b01056c69dc1b4a6bdf0
Behnam Bagheri <bagheri@linuxmail.org>
2025-03-08 21:51:10 +0330

    Commit files: .config/fish/functions/sudo.fish

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,7 +1,8 @@
 function sudo
     if test "$argv[1]" = "vim"
         command sudo -E nvim $argv[2..-1]  # Run nvim instead of vim while preserving the environment
-#     else
+    else
 #         command sudo -E $argv  # Run all other sudo commands normally while preserving the environment
+        command sudo $argv  # Run all other sudo commands normally while preserving the environment
     end
 end
