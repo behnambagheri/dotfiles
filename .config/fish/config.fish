a2b68727124005e81034be3508d030ff6c14c54f
Behnam Bagheri <bagheri@linuxmail.org>
2025-03-09 01:11:06 +0330

    Commit files: .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.local/dotfiles/variables.fish
 end
+
+if set -q SSH_TTY
+    echo "Welcome, you are connected via SSH!"
+    uptime
+
+    source ~/.local/dotfiles/variables.fish
+end
