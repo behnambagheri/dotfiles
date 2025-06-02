802ac939c8266918848fe1c39481c9f5f11fc274
Behnam Bagheri <bagheri@linuxmail.org>
2025-06-02 22:55:01 +0330

    Commit files: .local/dotfiles/variables.fish .ssh/config.d/bea

--- .local/dotfiles/variables.fish
+++ .local/dotfiles/variables.fish
@@ -1,7 +1,7 @@
 # Exclude SSH sessions from notifications
-set -U __done_exclude '^git (?!push|pull|fetch)'  '^ssh' '^vim'   '^sudo -Es' '^sudo su -' '^sudo vim' '^sudo crontab' '^crontab' '^watch' '^tail' '^sudo tail' '^kubectl exec' '^docker logs' '^docker compose logs' '^sudo docker logs' '^sudo docker compose logs' '^nvim' '^kubectl logs -f' '^kubectl get pods -o wide --watch' '^sudo -s$' '^htop$' '^glances$' '^bash$' '^sudo nvim' '^kubectl logs -f' '^docker compose stats$' '^docker stats$' '^kubectl edit' '^kubectl get pods --watch' '^sudo journalctl -xefu' '^journalctl -xefu' '^ncdu' '^man ' '^sudo ncdu ' '^knc' '^history' '^kubectl-ai' '^hx'
+set -U __done_exclude '^git (?!push|pull|fetch)' '^ssh' '^vim' '^sudo -Es' '^sudo su -' '^sudo vim' '^sudo crontab' '^crontab' '^watch' '^tail' '^sudo tail' '^kubectl exec' '^docker logs' '^docker compose logs' '^sudo docker logs' '^sudo docker compose logs' '^nvim' '^kubectl logs -f' '^kubectl get pods -o wide --watch' '^sudo -s$' '^htop$' '^glances$' '^bash$' '^sudo nvim' '^kubectl logs -f' '^docker compose stats$' '^docker stats$' '^kubectl edit' '^kubectl get pods --watch' '^sudo journalctl -xefu' '^journalctl -xefu' '^ncdu' '^man ' '^sudo ncdu ' '^knc' '^history' '^kubectl-ai' '^hx' '^sudo hx'
 # set -U __done_exclude '^(git (?!push|pull|fetch)|ssh|vim|nvim|sudo -Es|sudo su -|sudo (vim|crontab)|crontab|watch|tail|sudo tail|kubectl exec|docker (logs|compose logs)|sudo docker (logs|compose logs)|less|more|cat|journalctl|dmesg|htop|top|iotop|bmon|glances|man|ncdu|tmux|screen|nvim)$'
-set -U __done_min_cmd_duration 29000  # default: 5000 ms
+set -U __done_min_cmd_duration 29000 # default: 5000 ms
 set -U __done_allow_nongraphical 1
 
 set -Ux PREFERRED_EDITOR hx
@@ -23,20 +23,17 @@ set -U pisces_only_insert_at_eol 1
 
 set -Ux KUBECONFIG /Users/behnam/.kube/aircodeup.yaml:/Users/behnam/.kube/arvan.yaml:/Users/behnam/.kube/delta.yaml:/Users/behnam/.kube/novin.yaml:/Users/behnam/.kube/ilka.yaml:/Users/behnam/.kube/max-ir-central1-arvan.yaml
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
 
 # Load kubectl completions only if kubectl exists
 if command -q kubectl
-    kubectl completion fish > ~/.config/fish/completions/kubectl.fish
+    kubectl completion fish >~/.config/fish/completions/kubectl.fish
 end
-
 
 # Add /Users/behnam/.local/bin to PATH if it exists
 if test -d /Users/behnam/.local/bin
     set -Ux PATH $PATH /Users/behnam/.local/bin
 end
-
 
 # Add /Users/behnam/.local/bin to PATH if it exists
 if test -d /home/bea/.local/bin
@@ -45,9 +42,6 @@ end
 
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
-
 # source ~/.config/fish/conf.d/fzf.fish
 # source ~/.config/fish/conf.d/fifc.fish
 # source ~/.config/fish/conf.d/forgit.plugin.fish
-
-
