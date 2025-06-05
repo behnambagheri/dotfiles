f3a4a2e493b8e43ec6a3bf2dbaa08f350a238fb2
Behnam Bagheri <bagheri@linuxmail.org>
2025-06-05 14:50:11 +0330

    Commit files: .local/dotfiles/config_installer.sh

--- .local/dotfiles/config_installer.sh
+++ .local/dotfiles/config_installer.sh
@@ -36,7 +36,7 @@ PACKAGES=(
     nmap iputils-ping rsync netcat-traditional gcc build-essential
     net-tools iproute2 unzip bind9-utils prometheus-node-exporter
     ncdu nethogs jq python3-full python3-pip python3-venv ripgrep pipx eza
-    ninja-build gettext cmake unzip software-properties-common ripgrep chafa exa build-essential cmake
+    ninja-build gettext cmake unzip software-properties-common ripgrep chafa exa build-essential cmake libfuse2
 )
 # Parse script arguments
 for arg in "$@"; do
