85d42e383d5d50e1f83c949efbb0d161d31685f9
Behnam Bagheri <bagheri@linuxmail.org>
2025-06-09 14:04:58 +0330

    Commit files: .config/fish/functions/sudo.fish

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,14 +1,14 @@
 function sudo
-    if count $argv
-        switch $argv[1]
-            case l
-                command sudo lsd -lh $argv[2..-1]
-            case vim
-                command sudo hx $argv[2..-1]
-            case '*'
-                command sudo $argv
+        if test (count $argv) -gt 0
+                switch $argv[1]
+                        case l
+                                command sudo lsd -lh $argv[2..-1]
+                        case vim
+                                command sudo hx $argv[2..-1]
+                        case '*'
+                                command sudo $argv
+                end
+        else
+                command sudo
         end
-    else
-        command sudo
-    end
 end
