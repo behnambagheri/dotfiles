1288705378d60b76851508a1b6f4da9acadfcae9
Behnam Bagheri <bagheri@linuxmail.org>
2025-06-28 12:22:53 +0330

    Commit files: .config/fish/config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     source ~/.local/dotfiles/variables.fish
-    source ~/.config/fish/conf.d/fzf.fish
 end
 
 # if set -q SSH_TTY
