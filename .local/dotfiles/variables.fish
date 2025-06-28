751ba8b3ecfd64a1ac5977f18668d55957847cb6
Behnam Bagheri <bagheri@linuxmail.org>
2025-06-28 12:19:42 +0330

    Commit files: .local/dotfiles/variables.fish .ssh/config.d/bea .ssh/config.d/standalone.bitmax

--- .local/dotfiles/variables.fish
+++ .local/dotfiles/variables.fish
@@ -41,6 +41,6 @@ end
 
 set -U fish_user_paths $HOME/.local/bin $fish_user_paths
 
-# source ~/.config/fish/conf.d/fzf.fish
+source ~/.config/fish/conf.d/fzf.fish
 # source ~/.config/fish/conf.d/fifc.fish
 # source ~/.config/fish/conf.d/forgit.plugin.fish
