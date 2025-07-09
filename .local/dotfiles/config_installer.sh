dad91b68ac027cf329c396ea1c108ffffac3b95c
Behnam Bagheri <bagheri@linuxmail.org>
2025-07-09 11:34:21 +0330

    Commit files: .local/dotfiles/config_installer.sh

--- .local/dotfiles/config_installer.sh
+++ .local/dotfiles/config_installer.sh
@@ -13,7 +13,7 @@
 INSTALL_PROXY=false
 PUBLIC_PROXY=false
 
-
+ARCHITECTURE="$(dpkg --print-architecture)"
 
 # Define proxy settings
 PROXY="socks5h://127.0.0.1:7890"
@@ -209,7 +209,7 @@ install_fzf() {
 
   temp_dir=$(mktemp -d)
   cd "$temp_dir" >/dev/null || exit 1
-  wget -q https://github.com/junegunn/fzf/releases/download/v$REQUIRED_VERSION/fzf-${REQUIRED_VERSION}-linux_amd64.tar.gz
+  wget -q https://github.com/junegunn/fzf/releases/download/v$REQUIRED_VERSION/fzf-${REQUIRED_VERSION}-linux_"${ARCHITECTURE}".tar.gz
   tar xzf fzf-${REQUIRED_VERSION}-linux_amd64.tar.gz >/dev/null
   sudo mv fzf /usr/local/bin/ >/dev/null 2>&1
   cd - >/dev/null || exit
