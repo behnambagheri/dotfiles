290572941dc9e3272a86c0dacc2e2bb8fcff9e9b
Behnam Bagheri <bagheri@linuxmail.org>
2025-12-20 15:53:10 +0330

    Commit files: .config/fish/functions/config-update.fish

--- .config/fish/functions/config-update.fish
+++ .config/fish/functions/config-update.fish
@@ -2,4 +2,5 @@ function config-update
     config pull
     bash ~/.local/dotfiles/config_installer.sh $argv
     fisher remove meaningful-ooo/sponge gazorby/fish-abbreviation-tips wfxr/forgit 2> /dev/null || true
+    fisher update (fisher list)
 end
